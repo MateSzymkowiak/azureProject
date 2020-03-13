2a5559b681d71873f3dd4b5cd321daf96a098c5e
mszymkowiak <mateusz.szymkowiak@l4a-soft.com>
2020-03-13 12:05:09 +0100

    ReactSite

--- src/index.js
+++ src/index.js
@@ -1,12 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import './index.scss';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class Main extends React.Component {
+    render() {
+        return (
+            <div class="mainContainer">
+                <form>
+                    <div class="rssAndEmail">
+                        <input class="borders" value="https://test.com"disabled/>
+                        <input class="borders" placeholder="Email"/>
+                    </div>
+                    <div class="emailPreviewAndRssList">
+                        <div class="emailPreview borders">
+                            <p>
+                                Test
+                            </p>
+                        </div>
+                        <div class="rssList borders">
+                            <ul>
+                                <li>First Rss</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="submitButtons">
+                        <input class="borders" type="button" value={"Save"}/>
+                        <input class="borders" type="button" value={"Send"}/>
+                    </div>
+                </form>
+            </div>
+        );
+    }
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// ========================================
+
+ReactDOM.render(
+    <Main />,
+    document.getElementById('root')
+);
